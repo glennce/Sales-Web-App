658c80c4cda55566dfa17005521b0f05027141a5
glennce <157331142+glennce@users.noreply.github.com>
2025-08-24 21:29:26 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "sales-app-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/salesicon.png",
+  "/style.css"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
